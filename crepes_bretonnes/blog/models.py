dedae9030745de3b325df31114748cb2de4ee599
ayahetsiam <ayawaetsiam2004@gmail.com>
2026-02-11 18:56:19 +0000

    Models et migration suivi de la manipulation des données avec manage.py shell

--- crepes_bretonnes/blog/models.py
+++ crepes_bretonnes/blog/models.py
@@ -5,17 +5,7 @@ class Article(models.Model):
     titre = models.CharField(max_length=100)
     auteur = models.CharField(max_length=42)
     contenu = models.TextField(null=True)
-    realisateur = models.ForeignKey('Realisateur', on_delete=models.CASCADE, null=False)
     date = models.DateTimeField(auto_now_add=True, auto_now=False,verbose_name='Date de parution')
 
-# Enregistrement de l'article dans la base de données
-# Article(titre="Ma première crêpe", auteur="Jean Dupont", contenu="Voici comment faire une délicieuse crêpe bretonne.").save()
-
-# # Selection de tous les articles
-# articles = Article.objects.all()
-
-class Realisateur(models.Model):
-    pays= models.CharField(max_length=42)
-    nom = models.CharField(max_length=42)
-    prenom = models.CharField(max_length=42)
-    dateNaissance = models.DateField(auto_now=False, auto_now_add=False, verbose_name='Date de naissance')
+def __str__(self):
+    return self.titre
